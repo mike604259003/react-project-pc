1a13bc516dad7b6a742e0f043bfacfc4082e3f67
Mike <604259003@webmail.npru.ac.th>
2020-10-08 13:56:21 +0700

    Updates

---
+++ precache-manifest.3a7b89e7ea8c1dc600ffb11ee66e5dcc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5691f20c78444b55b135080833afb5",
+    "url": "/react-project-pc/index.html"
+  },
+  {
+    "revision": "cfa87fe9d569d0780f05",
+    "url": "/react-project-pc/static/css/2.4c90ec17.chunk.css"
+  },
+  {
+    "revision": "27dd83525853cb5ba1c5",
+    "url": "/react-project-pc/static/css/main.d8c1e859.chunk.css"
+  },
+  {
+    "revision": "cfa87fe9d569d0780f05",
+    "url": "/react-project-pc/static/js/2.3f95522d.chunk.js"
+  },
+  {
+    "revision": "89eb12b23a297500e9123b54a8fc8881",
+    "url": "/react-project-pc/static/js/2.3f95522d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27dd83525853cb5ba1c5",
+    "url": "/react-project-pc/static/js/main.a61230df.chunk.js"
+  },
+  {
+    "revision": "7b1eebe27a0c6b4852a0",
+    "url": "/react-project-pc/static/js/runtime-main.541645d9.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/react-project-pc/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/react-project-pc/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/react-project-pc/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/react-project-pc/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/react-project-pc/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/react-project-pc/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/react-project-pc/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/react-project-pc/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/react-project-pc/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/react-project-pc/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/react-project-pc/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/react-project-pc/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/react-project-pc/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/react-project-pc/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/react-project-pc/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/react-project-pc/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/react-project-pc/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/react-project-pc/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/react-project-pc/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/react-project-pc/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/react-project-pc/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/react-project-pc/static/media/team-1-800x800.53033970.jpg"
+  }
+]);
